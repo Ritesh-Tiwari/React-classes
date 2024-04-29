0594efd4a39ec2712c3776236ef8b03bb0387f90
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-04-29 17:36:28 +0530

    conditional rendering with multiple returns.

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -73,13 +73,13 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      ) : <p>We're still working on our menu. Please come back later :) </p>}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -125,12 +125,15 @@ function Footer() {
   // else alert("Sorry We're closed");
   return (
     <footer className="footer">
-      {isOpen && (
+
+      {/* ternaries operaters */}
+
+      {isOpen ? (
         <div className="order">
           <p>We're Open until {colseHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
-      )}
+      ) : (<p>We're happy to welcome you between {openHour}:00 and {colseHour}:00. </p>)}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open !");
