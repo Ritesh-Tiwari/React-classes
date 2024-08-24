ebe53bc99730a35cc639eaed3b61fa7756a9a34c
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-08-24 13:06:22 +0530

    Delete an item

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,24 +1,28 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "Charger", quantity: 1, packed: true },
-// ];
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 
 export default function App() {
   // check form functions
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
   function handleAddItems(item) {
     setItems(items => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id))
   }
 
   return (
     <div className="app">
       <Logo />
       <From onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
 
     </div>
@@ -67,13 +71,13 @@ function From({ onAddItems }) {
   </form>
 
 }
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {
           items.map(item =>
-            <Item item={item} key={item.id} />
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
           )}
 
       </ul>
@@ -81,13 +85,13 @@ function PackingList({ items }) {
   );
 
 }
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>✖️</button>
+      <button onClick={() => onDeleteItem(item.id)}>✖️</button>
     </li>
   );
 }
