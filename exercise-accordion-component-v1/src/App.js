94bc615340642849a0916353a64ea1400c0b2ad9
Ritesh Tiwari <tiwariritesh989@gmail.com>
2024-08-27 12:02:11 +0530

    have created a new project calculator

--- exercise-accordion-component-v1/src/App.js
+++ exercise-accordion-component-v1/src/App.js
@@ -28,20 +28,30 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
-      {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />)}
+      {data.map((el, i) =>
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >{el.text} </AccordionItem>
+      )}
     </div >
   );
 }
 
 
-function AccordionItem({ num, title, text }) {
-
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+  // const [isOpen, setIsOpen] = useState(false);
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    // setIsOpen((isOpen) => isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (<div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
@@ -49,7 +59,7 @@ function AccordionItem({ num, title, text }) {
     <p className="text">{title}</p>
     <p className="icon">{isOpen ? '-' : '+'}</p>
 
-    {isOpen && <div className="content-box">{text} </div>}
+    {isOpen && <div className="content-box">{children} </div>}
 
   </div>);
 }
